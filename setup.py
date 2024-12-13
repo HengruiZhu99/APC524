790cd00e4856c85de4eb8eb62df1a60546cc2c1a
Maria Fleury <mfleury@princeton.edu>
2024-12-13 08:36:00 -0500

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="navier_stokes",
-    version="0.1.0",  
-    description="for solving and comparing different solutions to 1D Navier Stokes", 
-    author="Hengrui Zhu, Doyup Kwon, Marie Joe Sawma, Zhan Wu and Maria Fleury",
-    package_dir={"": "src"},  
-    packages=find_packages(where="src"),  
-    python_requires=">=3.9, <4", # tuple typing was introduced in Python 3.9
-    install_requires=["numpy", "matplotlib"],
-)
+setup()
