1cee8f16369ff7b253b32045df98e000c44b5305
Zhan Wu <zw2812@princeton.edu>
2024-12-13 12:50:51 -0500

    Update __init__.py

--- src/navier_stokes/__init__.py
+++ src/navier_stokes/__init__.py
@@ -1,2 +1,10 @@
-from .fdm_fvm_functions import GaussionInitialize, SinusoidalInitialize, FDMMethod, FVMMethod, EulerIntegrator, RK2Integrator, RK4Integrator
+from .fdm_fvm_functions import (
+    GaussionInitialize,
+    SinusoidalInitialize,
+    FDMMethod,
+    FVMMethod,
+    EulerIntegrator,
+    RK2Integrator,
+    RK4Integrator,
+)
 from .plotting import plot_results, plot_kinetic_energy
